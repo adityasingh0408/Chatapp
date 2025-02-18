7a3423862584489499a7e3643c7ee88f634bdca3
adityasingh0408 <aditya2232005@gmail.com>
2025-02-18 10:40:10 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from 'path';
 
 import authroutes from "./route/authroute.js";
 import messageroute from "./route/messageroute.js"
@@ -11,10 +12,11 @@ import cookieParser from "cookie-parser";
 
 import{app , server} from "./socket/socket.js";
 
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotenv.config();
 
 // Middleware to parse JSON data
-const PORT = process.env.PORT || 5000;
 app.use(express.json()); // <-- Corrected with parentheses
 app.use(cookieParser());
 
@@ -29,10 +31,14 @@ app.use("/api/auth", authroutes);
 app.use("/api/message", messageroute);
 app.use("/api/users", userroute);
 
-
+app.use(express.static(path.join(__dirname,"/chatapp/dist")))
+app.get("#" , (req,res)=>{
+  res.sendFile(path.join(__dirname ,"chatapp", "dist" , "index.html"));
+})
 
 // Start the server and connect to MongoDB
 server.listen(PORT, () => {
   connecttomongodb();
   console.log(`Server running on port ${PORT}`);
 });
+ 
