3076ec11251c512be919e3a421d33a77e9119a51
adityasingh0408 <aditya2232005@gmail.com>
2025-02-18 12:04:43 +0530

    check gender

--- chatapp/src/pages/signup/signup.jsx
+++ chatapp/src/pages/signup/signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import CheckGender from './CheckGender'; 
+import CheckGender from './CheckGender.jsx';
 import { Link } from 'react-router-dom';
 import usesignup from '../../hooks/usesignup';
 
